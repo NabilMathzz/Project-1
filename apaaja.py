91b450d85d22256f34468172277784720ed6743e
NabilMathzz <utamaakun235@gmail.com>
2026-01-19 03:05:18 +0000

    versi 1.1 Odd Even

---
+++ apaaja.py
@@ -0,0 +1,8 @@
+bil = int(input("Bilangan dimasukin:"))
+          
+if bil % 2 == 0:
+    print (bil, "adalah Even")
+else :
+    print (bil, "adalah Odd")
+
+print("kode ini saya buat melalui codespaces")
